213d9e12b75d7aa64994bf842651bde861d4d9cd
Artem Ershov <ershov.artem.a@gmail.com>
2022-11-07 12:20:52 +0300

    Добавьте файлы проекта.

---
+++ cppm-homeWork.03.02/cppm-homeWork.03.02.cpp
@@ -0,0 +1,84 @@
+﻿
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+
+class Counter
+{
+
+private:
+
+	int a;
+
+public:
+	Counter(std::string q) {
+
+		if (q == "да") {
+			std::cout << "Введите начальное значение: ";
+			std::cin >> a;
+		}
+		else { a = 1; }
+	}
+
+	void add() { a++; }
+
+	void  subtract() { a--; }
+
+	void equal() { std::cout << a << std::endl; }
+
+	void set() { std::cin >> a; }
+
+
+};
+
+
+
+
+int main()
+	
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::string q;
+
+	std::cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
+	std::cin >> q;
+
+	Counter count(q);
+
+	char znak;
+
+	bool play = true;
+
+	while (play)
+	{
+		std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		std::cin >> znak;
+
+		switch (znak)
+		{
+		case '+':
+			count.add();
+			break;
+		case '-':
+			count.subtract();
+			break;
+		case '=':
+			count.equal();
+			break;
+		case 'x':
+			play = false;
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	std::cout << "До свидания!";
+
+}
+
+
